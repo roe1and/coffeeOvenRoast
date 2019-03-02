e0ce3f7cab398411e8eccc0aa815935c9cd48fb7
roe1and <schumies@gmx.co.uk>
2019-03-02 19:15:41 +0000

    tempUnit working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,50 +3,55 @@ import { Component, OnInit } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-import { Storage } from '@ionic/storage';
 
-import { AppService } from './app.service';
 import { NativeStorage } from '@ionic-native/native-storage/ngx';
 
 @Component({
   selector: 'app-root',
-  providers: [ AppService ],
+  providers: [  ],
   templateUrl: 'app.component.html'
 })
 export class AppComponent implements OnInit {
-  temp_unit;
+  temp_unit: string;
 
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private appService: AppService,
     private statusBar: StatusBar,
-    private storage: Storage,
     private nativeStorage: NativeStorage
-  ) {
-
-  }
+  ) { }
 
   ngOnInit() {
     this.initializeApp();
     console.log('opened');
-    this.platform.ready().then((readySource) => {
-      this.temp_unit = this.getTemperature();
+    this.platform.ready().then(() => {
+      this.getTemperature();
     });
-  }
-
-  ionViewWillEnter() {
+    console.log('init', this.temp_unit);
 
   }
 
   getTemperature() {
-    this.nativeStorage.getItem('tempUnit')
-    .then(
-      (temperatures) => {
-        this.temp_unit = temperatures == null ? 'celsius' : temperatures;
-        console.log('temp', this.temp_unit);
-      }
-    );
+  //  try {
+      this.nativeStorage.getItem('tempUnit')
+      .then(
+        (result) => {
+          this.temp_unit = result;
+        },
+        error => {
+          this.nativeStorage.setItem('tempUnit', 'celsius')
+          .then(
+          () => console.log('Stored item this.temp_unit =!'),
+          err => console.error('Error storing item', err)
+        );
+        this.temp_unit = 'celsius';
+
+          console.error('Error storing item', error);
+        }
+      );
+  //  } catch (e) {
+  //    console.log('e', e);
+  //  }
   }
 
   changeTempToggle() {
